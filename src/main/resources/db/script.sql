0c9732c38a1ca27caee815d5048ffc9c729fb7ff
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2024-07-11 11:53:20 -0300

    feat: add new tables establishment, parking, payment

--- src/main/resources/db/script.sql
+++ src/main/resources/db/script.sql
@@ -65,18 +65,52 @@
             conductor bigint
         ); 
 
-    alter table vehicle add constraint chk_fuel check (fuel in ('ALCOHOL', 'GASOLINE', 'NATURAL_GAS', 'ELECTRIC', 'HYDROGEN', 'FLEX_ALCOHOL_GASOLINE', 'HYBRID_ALCOHOL_GASOLINE_ELECTRIC'));
+        create table establishment(
+            id_establishment bigserial primary key,
+            name varchar(255) not null,
+            price_hour numeric(10,2) not null,
+            address bigint not null,
+            users bigint
+        );
 
-    alter table user_permission add constraint fk_user_permission_user foreign key (id_user) references users (id_user);
-    alter table user_permission add constraint fk_user_permission_permission foreign key (id_permission) references permission (id_permission);
-    alter table conductor add constraint fk_conductor_address foreign key (address) references address (id_address);
-    alter table conductor add constraint fk_conductor_users foreign key (users) references users (id_user);
-    alter table vehicle add constraint fk_vehicle_conductor foreign key (conductor) references conductor (id_conductor);
+        create table parking(
+            id_parking bigserial primary key,
+            time_fixed boolean not null,
+            time_start timestamp,
+            time_end timestamp,
+            total_time time,
+            total_price numeric(10,2),
+            vehicle bigint,
+            establishment bigint
+        );
 
-    insert into challenge.permission (description) values ('ADMINISTRATOR'), ('CONDUCTOR');
-    -- password 123456
-    INSERT INTO challenge.users (user_name,full_name,"password",account_non_expired,account_non_locked,credentials_non_expired,enabled) VALUES ('adm','adm','$2a$10$PqsrFKSSRev9lL0BMAE.IOvDB4r6plBA7c45UDzz4v0Wu1Es9XMs.',true,true,true,true);
-    INSERT INTO challenge.user_permission (id_user,id_permission) VALUES (1,1), (1,2);
+        create table payment(
+            id_payment bigserial primary key,
+            create_date timestamp not null,
+            payment_date timestamp not null,
+            payment_format varchar(11) not null,
+            payment_completed boolean not null,
+            parking bigint
+        );
+
+        alter table vehicle add constraint chk_fuel check (fuel in ('ALCOHOL', 'GASOLINE', 'DIESEL', 'NATURAL_GAS', 'ELECTRIC', 'HYDROGEN', 'FLEX_ALCOHOL_GASOLINE', 'HYBRID_ALCOHOL_GASOLINE_ELECTRIC'));
+        alter table payment add constraint chk_payment_format check (payment_format in ('CREDIT_CARD', 'DEBIT_CARD', 'PIX'));
+
+        alter table user_permission add constraint fk_user_permission_user foreign key (id_user) references users (id_user);
+        alter table user_permission add constraint fk_user_permission_permission foreign key (id_permission) references permission (id_permission);
+        alter table conductor add constraint fk_conductor_address foreign key (address) references address (id_address);
+        alter table conductor add constraint fk_conductor_users foreign key (users) references users (id_user);
+        alter table vehicle add constraint fk_vehicle_conductor foreign key (conductor) references conductor (id_conductor);
+        alter table establishment add constraint fk_establishment_address foreign key (address) references address (id_address);
+        alter table establishment add constraint fk_establishment_users foreign key (users) references users (id_user);
+        alter table parking add constraint fk_parking_vehicle foreign key (vehicle) references vehicle (id_vehicle);
+        alter table parking add constraint fk_parking_establishment foreign key (establishment) references establishment (id_establishment);
+        alter table payment add constraint fk_payment_parking foreign key (parking) references parking (id_parking);
+
+        insert into challenge.permission (description) values ('ADMINISTRATOR'), ('CONDUCTOR'), ('OWNER');
+        -- password 123456
+        insert into challenge.users (user_name,full_name,"password",account_non_expired,account_non_locked,credentials_non_expired,enabled) VALUES ('adm','adm','$2a$10$PqsrFKSSRev9lL0BMAE.IOvDB4r6plBA7c45UDzz4v0Wu1Es9XMs.',true,true,true,true);
+        insert into challenge.user_permission (id_user,id_permission) VALUES (1,1), (1,2), (1,3);
 
 --     end if;
 -- end $$;
